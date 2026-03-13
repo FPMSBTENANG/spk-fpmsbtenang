71302879238222ad2aa3c42da5831b91de6c1ed5
FPMSBTENANG <tenang.fpm@gmail.com>
2026-03-13 17:48:16 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -4,7 +4,7 @@
 // ⚠️ AMARAN PENTING: 
 // Gantikan URL di bawah dengan URL Web App Google Apps Script kau yang sebenar!
 // Pastikan kau copy URL yang berakhir dengan /exec
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzQ01SBddVoDkl6NIFHp24TOlRk8CRNZNgI-rjEDDiQDRa36ZkRnkX0Z_P5db_gtpuf/exec"; 
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzKPt8eZEb8QfYvcydCkn_KgCz5P4_7ZUfXNy1TQt3wDQ2fkW9jUuHFl70o0PsSgakP/exec"; 
 
 /**
  * Fungsi utama untuk menghantar arahan dan data ke Backend
@@ -39,5 +39,6 @@ async function panggilAPI(action, data) {
     }
 
 }
+
 
 
