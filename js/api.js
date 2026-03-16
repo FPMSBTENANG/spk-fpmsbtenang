aef2e63048af86dc9f1d5d5e26f4fae6ce44aab8
FPMSBTENANG <tenang.fpm@gmail.com>
2026-03-16 15:53:55 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,12 +1,12 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxL1Duovef8PfrK0nKyk4aSOltbkm1-6BOK69PM82PX6YdgkYwWJavUYWBkxuW4zWg1/exec"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbyw0clbAErEKjhGtaQSFFwvmXVGwd10QpdZiFAnsWIxHntc3nEo_oEpQzHU460CfNg0/exec";
 
 async function panggilAPI(action, data = {}) {
-    
+
     const sesiUser = JSON.parse(sessionStorage.getItem('spk_user'));
-    const publicActions = ['login', 'register', 'resetPassword'];
+    const publicActions = ['login', 'register', 'requestResetPin', 'verifyResetPin'];
 
     if (!publicActions.includes(action)) {
-        
+
         if (sesiUser && sesiUser.token_sesi && sesiUser.email) {
             data.token_sesi = sesiUser.token_sesi;
             data.email_sesi = sesiUser.email;
@@ -26,18 +26,18 @@ async function panggilAPI(action, data = {}) {
     try {
         const response = await fetch(API_URL, {
             method: 'POST',
-            body: JSON.stringify({ 
-                action: action, 
-                payload: data 
+            body: JSON.stringify({
+                action: action,
+                payload: data
             })
         });
         return await response.json();
-        
+
     } catch (error) {
         console.error("Ralat Rangkaian API:", error);
-        return { 
-            status: false, 
-            message: "Gagal berhubung dengan pelayan. Sila semak sambungan internet anda." 
+        return {
+            status: false,
+            message: "Gagal berhubung dengan pelayan. Sila semak sambungan internet anda."
         };
     }
 }
